b2c1ca6d05b8cd9c8935a40ffc7de07da632f298
Justin Suess <justinsuess@sbcglobal.net>
2019-01-08 20:51:00 -0600

    changed doubleField

--- DoubleField.java
+++ DoubleField.java
@@ -1,9 +1,15 @@
 
-public class DoubleField implements Field {
-    
-    private String name;
-    
-    public String getName() {
-        
+public class DoubleField extends Field {
+
+    private double value;
+
+    /**
+     * Create a new double field with the name name and the value val
+     * @param name The name of the field
+     * @param val The value to set it to.
+     */
+    public DoubleField(String name, double val) {
+        super(name);
+        value = val;
     }
 }
