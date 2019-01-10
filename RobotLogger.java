338f9f7e0119e3c8c853eb6e984ead46ac26ed46
Justin Suess <justinsuess@sbcglobal.net>
2019-01-09 19:02:28 -0600

    Added setLevel and logMessage to RobotLogger

--- RobotLogger.java
+++ RobotLogger.java
@@ -6,8 +6,26 @@ public class RobotLogger {
     /**
      * Flushes all logged updates to a file.
      */
+
     public void flush() {
         
+    }
+
+    /**
+     * Logs a raw message to the console
+     * @param message The message to log
+     * @param messageLevel The level of the message to log.
+     */
+    public void logMessage(String message, Level messageLevel) {
+        mainLogger.log(messageLevel, message);
+    }
+
+    /**
+     * Sets the level of messages to be logged.
+     * @param newLevel The level in question.
+     */
+    public void setLevel(Level newLevel) {
+        mainLogger.setLevel(newLevel);
     }
 
 }
