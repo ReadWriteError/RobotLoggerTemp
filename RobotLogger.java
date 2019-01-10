39a028c7fcf338df8848d779c55ccdb3d2a22ff0
Justin Suess <justinsuess@sbcglobal.net>
2019-01-09 19:13:08 -0600

    Added timestamp and raw logging code

--- RobotLogger.java
+++ RobotLogger.java
@@ -3,21 +3,29 @@ import java.util.logging.*;
 public class RobotLogger {
 
     private Logger mainLogger = Logger.getLogger(RobotLogger.class.getName());
+
     /**
      * Flushes all logged updates to a file.
      */
-
     public void flush() {
         
     }
 
+    private void createRawEntry(String message, Level messageLevel) 
+    {
+        mainLogger.log(messageLevel, "[" + getTimestampString() + "]" + message);
+    }
+    //XXX - Should return the match time, not epoch time.
+    private String getTimestampString() {
+        return Long.toString(System.currentTimeMillis());
+    }
     /**
      * Logs a raw message to the console
      * @param message The message to log
      * @param messageLevel The level of the message to log.
      */
     public void logMessage(String message, Level messageLevel) {
-        mainLogger.log(messageLevel, message);
+        createRawEntry("[MESSAGE] " + message, messageLevel);
     }
 
     /**
