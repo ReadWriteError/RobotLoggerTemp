05c309ecdccd14caacbccb635a679e9bdbfe815c
Justin Suess <justinsuess@sbcglobal.net>
2019-01-09 18:41:42 -0600

    Changed doublefield to have a formatter and a toString() method

--- DoubleField.java
+++ DoubleField.java
@@ -1,7 +1,11 @@
+import java.text.DecimalFormat;
 
 public class DoubleField extends Field {
 
     private double value;
+
+    //Changes how the decimal is formatted as a string.
+    private static final DecimalFormat df = new DecimalFormat("#.00"); 
 
     /**
      * Create a new double field with the name name and the value val
@@ -11,5 +15,12 @@ public class DoubleField extends Field {
     public DoubleField(String name, double val) {
         super(name);
         value = val;
+    }
+    /**
+     * Returns a formatted decimal
+     * @return The formatted decimal.
+     */
+    public String toString() {
+        return df.format(value);
     }
 }
